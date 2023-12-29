fc88f49796e6bafc7f563a21c543902af44e4b9c
suryadi ismail <suryadiismail71@gmail.com>
2023-12-29 14:25:51 +0700

    update config

--- vite.config.js
+++ vite.config.js
@@ -24,6 +24,12 @@ export default defineConfig({
             src: 'icon-512x512.png',
             sizes: '512x512',
             type: 'image/png'
+          },
+          {
+            src: 'maskable-icon.png',
+            sizes: '196x196',
+            type: 'image/png',
+            purpose: "any maskable"
           }
         ]
       }
