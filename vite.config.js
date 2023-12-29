5dd0c81c08a7c0153856f6506f8aa2ae681ecfc4
suryadi ismail <suryadiismail71@gmail.com>
2023-12-29 12:48:41 +0700

    edit pwa

--- vite.config.js
+++ vite.config.js
@@ -7,9 +7,7 @@ import { VitePWA } from 'vite-plugin-pwa';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(), VitePWA({ registerType: 'autoUpdate', devOptions: {
-      enabled: true
-    } })
+    vue(), VitePWA()
   ],
   resolve: {
     alias: {
